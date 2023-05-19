7dbcf32cae02e3074f42de352350fbef368d79df
Asmaa-Refat <asmaarefat071@gmail.com>
2023-05-19 19:08:03 +0300

    missing slash

--- RegistrationSys_AdminUI/src/Main.java
+++ RegistrationSys_AdminUI/src/Main.java
@@ -41,8 +41,8 @@ public class Main {
         System.out.println("Number of verified batch files: "+verifiedNumber);
     }
     public static void main(String[] args) {
-        Model.databaseFilePath = "app/data/database";
-        Model.readFolderPath = "app/data/batch/";
+        Model.databaseFilePath = "/app/data/database";
+        Model.readFolderPath = "/app/data/batch/";
         printStudentsStat();
         printNumberOfFiles();
         printNumberOfVerifiedFiles();
